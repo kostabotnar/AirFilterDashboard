43cba3e565f3f9fc285b21aff220e690f9f7c13a
kostia.botnar <kosta.botnar@gmail.com>
2025-04-03 16:37:09 -0500

    Add logs Filter metadata samples with the metagenomic data

--- clean_sample_metadata.py
+++ clean_sample_metadata.py
@@ -1,19 +1,32 @@
+from pathlib import Path
+
 import pandas as pd
 
 
-def main():
+def clean_metadata():
+    print("Read Metadata file")
     df = pd.read_csv('data/Sample Metadata.csv', parse_dates=['Collection Date'])
     df = df.dropna(subset=['Sample ID', 'Collection Date', 'Location (on dish)'])
 
+    print("Read Regions file")
     df_reg = pd.read_csv('data/Regions.csv', usecols=['Location', 'Region'])
     df['Location (on dish)'] = df['Location (on dish)'].str.upper()  # Convert location names to uppercase
+
+    print("Merge Regions with Metadata")
     df = (df.merge(df_reg, left_on='Location (on dish)', right_on='Location').
           drop(columns=['Location', 'Location (on dish)']))
     df = df.fillna("Unknown")
     df['Sample ID'] = df['Sample ID'].str.upper()  # Remove leading/trailing whitespace from sample IDs
-    df.to_csv('build/Sample Metadata.csv', index=False)
 
+    if Path('build/Sample Abundances.csv').exists():
+        print("Filter samples with metagenomic data")
+        samples = pd.read_csv('build/Sample Abundances.csv', usecols=['Sample ID']).drop_duplicates()
+        df = df.merge(samples, on='Sample ID')
+
+    print("Write Sample Metadata to CSV")
+    df.to_csv('build/Sample Metadata.csv', index=False)
+    print("Sample Metadata merged successfully.")
 
 
 if __name__ == "__main__":
-    main()
+    clean_metadata()
--- prepare_reads_data.py
+++ prepare_reads_data.py
@@ -2,11 +2,18 @@ import pandas as pd
 
 
 def main():
+    """
+    Reads the fastq_stats.tsv file, extracts sample IDs, and formats the data for easier downstream analysis.
+    """
+    print("Reading fastq_stats.tsv...")
     df = pd.read_csv("data/fastq_stats.tsv", sep="\t")
+    print("Extracting sample IDs...")
     df["Sample"] = df["Sample"].apply(lambda x: x.split(".")[0])
     df.columns = ["Sample ID", "Number of Reads", "Mean Read Length"]
     df['Sample ID'] = df['Sample ID'].str.upper()
+    print("Saving to Sample Read Stats.csv...")
     df.dropna().to_csv("build/Sample Read Stats.csv", index=False)
+    print("Sample Read Stats.csv saved successfully.")
 
 if __name__ == "__main__":
     main()
