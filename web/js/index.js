8bfeb5ceea46e27233d50471594b037f9c3787ca
kostia.botnar <kosta.botnar@gmail.com>
2025-06-20 16:29:37 -0500

    change the dashboard url

--- web/js/index.js
+++ web/js/index.js
@@ -1,4 +1,4 @@
-const powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiNGFhMmM2ZmUtZTA4Yy00MDlmLWI0NDMtODNkM2RjY2UyODhiIiwidCI6IjdiZWYyNTZkLTg1ZGItNDUyNi1hNzJkLTMxYWVhMjU0Njg1MiIsImMiOjN9";
+const powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiZTA3YzM5NzMtMjVkOS00N2UxLTk5ZjctMmQ3N2E3MjQyMzYzIiwidCI6IjdiZWYyNTZkLTg1ZGItNDUyNi1hNzJkLTMxYWVhMjU0Njg1MiIsImMiOjN9";
 const pswHash = "66f22348ba8c3cff0ccadfba7b2c7d6bf8f434cba1e28430d2df3f0c79d3941fc90459952dd5273be4a8d05a36966124c159599ace8a2faaaa301bbe9e071745";
 
 function togglePasswordVisibility() {
